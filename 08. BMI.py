91c8e1a66edfd62aa0a9c2c0f16abf812ff15be1
LxiTxnISTLG <sergio.endara@tecnologicoliceocristiano.edu.ec>
2024-07-29 04:04:05 -0500

    commit

---
+++ 08. BMI.py
@@ -0,0 +1,12 @@
+# Write code below 💖
+
+height = 2 ** 2
+kg = 1
+mass = kg
+
+height = float(input("Set ur height: "))
+mass =float(input("Set ur mass in kg: "))
+
+BMI = (mass) / height
+
+print("Your BMI is: ", (BMI))
